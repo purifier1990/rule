3634f33b8bbbbd79c385e9e41aaa28ec2a1d7e21
purifier1990 <wenyuzhao@vmware.com>
2016-04-20 20:27:15 +0800

    add data module class

---
+++ rule/card.swift
@@ -0,0 +1,20 @@
+//
+//  card.swift
+//  rule
+//
+//  Created by wenyuzhao on 4/20/16.
+//  Copyright © 2016 wenyuzhao. All rights reserved.
+//
+
+import UIKit
+
+class card  {
+   
+   var cardImage: UIImage
+   
+   
+   init(image: UIImage) {
+      self.cardImage = image
+   }
+   
+}
